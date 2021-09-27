750e49cbccd7cc02df723b5d812410673f6a890b
AssassinGhost <saurabhsharma.karan@gmail.com>
2021-09-27 16:05:33 +0530

    Added the Reverse Array Project

---
+++ Reverse_Array.py
@@ -0,0 +1,13 @@
+def reverse_array (array1, start, end):
+    while (start<=end):
+        array1[start], array1[end] = array1[end], array1[start]
+        start += 1
+        end -= 1
+
+A = [1, 2, 2 , 3, 4]
+start=0
+end= len(A) -1
+print(A)
+print("The reversed Array is: ")
+reverse_array(A, start, end)
+print(A)
